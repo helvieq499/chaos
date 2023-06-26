79151dcb17b586e6c818411be11317630950eaba
helvieq499 <73959954+helvieq499@users.noreply.github.com>
2023-06-25 22:50:25 -0400

    load credentials from local storage

--- src/logic/client/credentials.rs
+++ src/logic/client/credentials.rs
@@ -3,3 +3,11 @@ pub struct Credentials {
     pub token: String,
     pub is_bot: bool,
 }
+
+impl Credentials {
+    pub fn from_local_storage() -> Option<Self> {
+        crate::utils::local_storage::get()
+            .and_then(|local_storage| local_storage.get_item("credentials").ok().flatten())
+            .and_then(|creds| serde_json::from_str(&creds).ok())
+    }
+}
--- src/logic/client/mod.rs
+++ src/logic/client/mod.rs
@@ -1,13 +1,15 @@
 use leptos::*;
 use std::{rc::Rc, sync::RwLock};
 
+use self::credentials::Credentials;
+
 pub mod credentials;
 
 pub struct Client {
     pub connect: RwSignal<bool>,
     pub sequence: RwLock<Option<i32>>,
 
-    pub credentials: RwSignal<Option<credentials::Credentials>>,
+    pub credentials: RwSignal<Option<Credentials>>,
 }
 
 impl Client {
@@ -16,7 +18,7 @@ impl Client {
             connect: create_rw_signal(cx, false),
             sequence: RwLock::new(None),
 
-            credentials: create_rw_signal(cx, None),
+            credentials: create_rw_signal(cx, Credentials::from_local_storage()),
         }
     }
 
