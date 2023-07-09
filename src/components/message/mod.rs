69e31cb75903df7201b0cc0e7d34df847251b36f
helvieq499 <73959954+helvieq499@users.noreply.github.com>
2023-07-09 02:02:21 -0400

    add markdown parser

--- src/components/message/mod.rs
+++ src/components/message/mod.rs
@@ -1,9 +1,35 @@
+use std::sync::OnceLock;
+
 use crate::logic::types::Message;
+use comrak::{ComrakExtensionOptions, ComrakOptions, ComrakParseOptions, ComrakRenderOptions};
 use leptos::*;
 
 pub mod list;
 
 #[component]
 pub fn ListedMessage(cx: Scope, message: Message) -> impl IntoView {
-    view! { cx, <div>{message.content}</div> }
+    static OPTIONS: OnceLock<ComrakOptions> = OnceLock::new();
+
+    let message_html = comrak::markdown_to_html(
+        &message.content,
+        OPTIONS.get_or_init(|| ComrakOptions {
+            extension: ComrakExtensionOptions {
+                strikethrough: true,
+                autolink: true,
+                tasklist: true,
+                ..Default::default()
+            },
+            parse: ComrakParseOptions {
+                smart: false,
+                default_info_string: None,
+                relaxed_tasklist_matching: false,
+            },
+            render: ComrakRenderOptions {
+                hardbreaks: true,
+                ..Default::default()
+            },
+        }),
+    );
+
+    view! { cx, <div inner_html=message_html/> }
 }
