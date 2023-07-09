9147a47f44f4e3922c7381ea5ce0e0148537a86a
helvieq499 <helvieq499@users.noreply.github.com>
2023-07-09 08:52:30 +0000

    deploy: fb219482835a7254d8ffd2b95e1b41640e4151c3

--- chaos-1d00fbfb0b841d0d.js
+++ chaos-1d00fbfb0b841d0d.js
@@ -1165,7 +1165,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('chaos-431cc1a6c109e106_bg.wasm', import.meta.url);
+        input = new URL('chaos-1d00fbfb0b841d0d_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
